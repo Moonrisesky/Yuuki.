1669d44fd7ffbbcf92b2616b04bbe314a3b13d79
tufo-discord <50560502+tufo-discord@users.noreply.github.com>
2019-05-25 17:57:57 +0300

    Update Yuuki.js

--- Yuuki.js
+++ Yuuki.js
@@ -1,1 +1,24 @@
 
+const discord = require("discord.js");
+const config = require("./config.json");
+const bot = new discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is ready for action!`);
+  bot.user.setActivity("Minekwaft")
+});
+
+bot.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = config.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}help`) {
+      return message.channel.send("I'm Don't have help . :cry:");
+});
+
+bot.login(config.token);
